7d2e6db18a5b5c63ae2f94eafd00e616c9139847
Artem <bondarchuk.artem20@gmail.com>
2022-02-16 11:35:20 +0200

    non interactive mode implementation

--- solver.go
+++ solver.go
@@ -18,7 +18,7 @@ func inMode () []float64 {
 		fmt.Printf("%s: ", paramNames[len(parameters)])
 		param, err := reader.ReadString('\n')
 		if err != nil {
-			fmt.Println("An error occured while reading input. Please try again", err)
+			fmt.Println("Error. Failed to read input. Please try again", err)
 			continue
 		}
 
@@ -41,6 +41,48 @@ func inMode () []float64 {
 
 func nonInMode (fileName string) []float64 {
 	var parameters []float64
+
+	file, err := os.Open(fileName)
+	if err != nil {
+		fmt.Println("Error occured while trying to open file.", err)
+		os.Exit(1)
+	}
+	defer file.Close()
+	
+	scanner := bufio.NewScanner(file)
+	
+	var line string
+	for scanner.Scan() {
+        line = scanner.Text()
+    }
+	if err := scanner.Err(); err != nil {
+        fmt.Println("Error occured while trying to scan file.", err)
+		os.Exit(1)
+    }
+	
+	line = strings.Trim(line, " ")
+	paramSplit := strings.Split(line, " ")
+
+	if len(paramSplit) != 3 { 
+		fmt.Printf("Error. Wrong number of parameters. Must be 3\n")
+		os.Exit(1)
+	}
+
+	for i, val := range paramSplit {
+		valFloat, err := strconv.ParseFloat(val, 64)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Error code = %s\n", err)
+			os.Exit(1)
+		}
+
+		if valFloat == 0 && i == 0 {
+			fmt.Fprint(os.Stderr, "Error. a cannot be 0\n")
+			os.Exit(1)
+		}
+		parameters = append(parameters, valFloat)
+	}
+	fmt.Println(paramSplit)
+
 	return parameters
 }
 
@@ -80,12 +122,13 @@ func main () {
 	}
 	
 	results = equation(parameters)
+	fmt.Printf("Equation is: (%.2f) x^2 + (%.2f) x + (%.2f) = 0\n", 
+		parameters[0], parameters[1], parameters[2])
 
-	if len(results) == 0 {
-		fmt.Printf("There are 0 roots");
-	} else if len(results) == 1 {
-		fmt.Printf("x1 = %f", results[0]);
-	} else {
-		fmt.Printf("x1 = %f\nx2 = %f", results[0], results[1]);
+	if len(results) == 1 {
+		fmt.Printf("x1 = %f\n", results[0])
+	} else if len(results) == 2 {
+		fmt.Printf("x1 = %f\nx2 = %f\n", results[0], results[1])
 	}
+	fmt.Printf("There are %d root(s)", len(results))
 }
